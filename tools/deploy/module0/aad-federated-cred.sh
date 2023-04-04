18b88fb6ccf6ad705161e6c6c5da45b98718d560
Ahmed Bham <ahmed.bham@gmail.com>
2023-04-04 18:27:11 +0000

    fixes to module0 and 2

--- tools/deploy/module0/aad-federated-cred.sh
+++ tools/deploy/module0/aad-federated-cred.sh
@@ -5,18 +5,20 @@ if [ -z "$github_username" ]; then
 	exit 1
 fi
 
+sed -i "s/your-github-username/$github_username/g" tools/deploy/module0/credential.json
+
 # Create an Azure AD application
 uniqueAppName=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c10 ; echo '')
 appId=$(az ad app create --display-name $uniqueAppName --query appId --output tsv)
 echo "appId is $appId"
 
 # Create a service principal for the Azure AD app.
-assigneeObjectId=$(az ad sp create --id $appId --query id --output tsv)
+assigneeObjectId=$(az ad sp show --id $appId --query id --output tsv)
 
 # Create a role assignment for the Azure AD app.
 subscriptionId=$(az account show --query id --output tsv)
 az role assignment create --role Contributor --subscription $subscriptionId --assignee-object-id  $assigneeObjectId --assignee-principal-type ServicePrincipal --scope /subscriptions/$subscriptionId/resourceGroups/$resourceGroupName
 
 # Create a federated identity credential on the Azure AD app.
-az ad app federated-credential create --id $appId --parameters {"name":"openai-levelup-oidc","issuer":"https://token.actions.githubusercontent.com","subject":"repo:$github_username/azure-openai-api-levelup:ref:refs/heads/main","audiences":["api://AzureADTokenExchange"],"description":"Workload Identity for Azure OpenAI API LevelUp repo"}
+az ad app federated-credential create --id $appId --parameters tools/deploy/module0/credential.json
 
